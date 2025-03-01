d12c60ca4166cad8412078d8c4fa5d617c765804
archbaer <pereirakebaili@gmail.com>
2025-03-01 13:43:27 +0000

    jpa settings

--- backend-server/src/main/java/com/totos/backend_server/models/User.java
+++ backend-server/src/main/java/com/totos/backend_server/models/User.java
@@ -1,43 +1,41 @@
 package com.totos.backend_server.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User implements UserDetails {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
+    @Getter
     @Column(nullable = false, unique = true)
     private String username;
 
+    @Setter
+    @Getter
     @Column(nullable = false)
     private String password;
 
+    @Setter
+    @Getter
     @Column(nullable = false, unique = true)
     private String email;
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
 
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
 }
